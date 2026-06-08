01f71a98284e7ab2b7e87d59030afbde58056531
LucasDutraFig <lucasdfigueiredo2016@gmail.com>
2026-06-07 21:40:33 -0300

    jsonserver

--- codigo/index.js
+++ codigo/index.js
@@ -13,16 +13,21 @@
 // Autor: Rommel Vieira Carneiro
 // Data: 03/10/2023
 
+const path = require('path')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./db/db.json')
+const router = jsonServer.router(path.join(__dirname, 'db', 'db.json'))
   
 // Para permitir que os dados sejam alterados, altere a linha abaixo
 // colocando o atributo readOnly como false.
-const middlewares = jsonServer.defaults({ noCors: true })
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, 'public'),
+  noCors: true
+})
 server.use(middlewares)
 server.use(router)
 
-server.listen(3000, () => {
-  console.log(`JSON Server is running em http://localhost:3000`)
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log(`JSON Server is running em http://localhost:${port}`)
 })
